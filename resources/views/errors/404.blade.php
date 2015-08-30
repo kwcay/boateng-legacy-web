7b6ce940f885f56992b07175f599f8e6d8fcc429
frnkly <frank@frnk.ca>
2015-08-30 02:29:35 -0400

    Added custom HTTP error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <title>Di Nkomo: the book of native tongues.</title>
+    <meta http-equiv="expires" content="-1">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="cache-control" content="no-cache" />
+	<meta name="robots" content="noindex, follow" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    html, body {
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        color: #333;
+        font: normal 21px/1.618 'sinanova', Serif;
+        background-color: #f7f7f7;
+        text-align: center;
+    }
+    header {
+        width: 100%;
+        display: block;
+        letter-spacing: 10px;
+        text-align: center;
+        font-size: 36px;
+        color: #aaa;
+    }
+    section {
+        width: 50%;
+        margin: 50px auto 0;
+    }
+    .large {
+        font-size: 1.2em;
+    }
+    </style>
+</head>
+<body>
+	<header>~·~</header>
+    <section>
+        <div class="large">404</div>
+        Not Found.
+    </section>
+</body>
+</html>
