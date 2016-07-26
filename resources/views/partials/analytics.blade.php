41b239fb3c0a8b8046b7840289d017133004135c
frnkly <frank@frnk.ca>
2016-07-26 22:15:19 +0000

    Updated Keen.io script

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -8,7 +8,7 @@
   !function(a,b){a("Keen","https://d26b395fwzu5fz.cloudfront.net/3.4.1/keen-tracker.min.js",b)}(function(a,b,c){var d,e,f;c["_"+a]={},c[a]=function(b){c["_"+a].clients=c["_"+a].clients||{},c["_"+a].clients[b.projectId]=this,this._config=b},c[a].ready=function(b){c["_"+a].ready=c["_"+a].ready||[],c["_"+a].ready.push(b)},d=["addEvent","setGlobalProperties","trackExternalLink","on"];for(var g=0;g<d.length;g++){var h=d[g],i=function(a){return function(){return this["_"+a]=this["_"+a]||[],this["_"+a].push(arguments),this}};c[a].prototype[h]=i(h)}e=document.createElement("script"),e.async=!0,e.src=b,f=document.getElementsByTagName("script")[0],f.parentNode.insertBefore(e,f)},this);
 </script>
 <script type="text/javascript">
-  window.keen = new Keen({
+  var Keen = new Keen({
     projectId: "{{ env('KEEN_PROJECT_ID') }}",
     writeKey: "{{ env('KEEN_WRITE_KEY') }}",
     protocol: "auto",
