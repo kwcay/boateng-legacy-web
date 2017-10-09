b1e9a7259c491ce5e9efb9e3685e1bc296ef3ab2
Frank <frank@frnk.ca>
2017-10-09 13:46:03 -0400

    Tracker service provider + config

--- config/services.php
+++ config/services.php
@@ -1,38 +1,10 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
-    | default location for this type of information, allowing packages
-    | to have a conventional place to find your various credentials.
-    |
-    */
-
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+    'keen' => [
+        'id'     => env('KEEN_PROJECT_ID'),
+        'read'   => env('KEEN_READ_KEY'),
+        'write'  => env('KEEN_WRITE_KEY'),
+        'master' => env('KEEN_MASTER_KEY'),
     ],
-
-    'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => 'us-east-1',
-    ],
-
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
-    ],
-
-    'stripe' => [
-        'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-    ],
-
 ];
