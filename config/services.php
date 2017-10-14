7167fd49dbc32033ef141483b47d6ad3f550aeff
Frank <frank@frnk.ca>
2017-10-13 21:15:34 -0400

    Updated Keen config

--- config/services.php
+++ config/services.php
@@ -5,5 +5,9 @@ return [
         'id'     => env('KEEN_PROJECT_ID'),
         'master' => env('KEEN_MASTER_KEY'),
         'write'  => env('KEEN_WRITE_KEY'),
+        'addons' => [
+            'geo-data'  => 1,   // IP to Geo parser
+            'user-data' => 1,   // User Agent parser
+        ],
     ],
 ];
