a58ad52485447b151d29a5f38b2e83c6bb2c120a
Frank <frank@frnk.ca>
2017-11-06 09:02:28 -0500

    WIP

--- resources/views/language/learn/index.blade.php
+++ resources/views/language/learn/index.blade.php
@@ -13,6 +13,16 @@
 
     <div class="row">
         <div class="col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-3">
+            We're building a <strong>free</strong> language learning tool for
+            <strong><a href="{{ route('language.show', $lang->code) }}">{{ $lang->name }}</a></strong>.
+
+            @include('language.learn.signup', ['name' => $lang->name])
+            <hr>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-3">
             @lang('branding.pitch')
             <hr>
         </div>
---
+++ resources/views/language/learn/signup.blade.php
@@ -0,0 +1,43 @@
+
+<div id="mc_embed_signup">
+    <form
+        action="https://doraboateng.us15.list-manage.com/subscribe/post?u=186494e222668e5e50ccf5b89&amp;id=0ebf6a2000"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        class="validate form edit"
+        target="_blank"
+        novalidate>
+
+        <div id="mc_embed_signup_scroll">
+            <div class="mc-field-group">
+                <label for="mce-EMAIL">Email Address </label>
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-NAME">Name </label>
+                <input type="text" value="" name="NAME" class="required" id="mce-NAME">
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-LANGUAGE">Language </label>
+                <select name="LANGUAGE" class="" id="mce-LANGUAGE">
+                    <option value=""></option>
+                    <option value="English">English</option>
+                    <option value="Français">Français</option>
+
+                </select>
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-MMERGE4">Languages of Interest </label>
+                <input type="text" value="{{ $name }}" name="MMERGE4" class="" id="mce-MMERGE4">
+            </div>
+            <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_186494e222668e5e50ccf5b89_0ebf6a2000" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+    </form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='NAME';ftypes[1]='text';fnames[3]='LANGUAGE';ftypes[3]='dropdown';fnames[4]='MMERGE4';ftypes[4]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
