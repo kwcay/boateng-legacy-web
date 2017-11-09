58a61fe0b054264c70ad01f986036310f1759955
Frank <frank@frnk.ca>
2017-11-08 19:51:28 -0500

    Updating composer

--- config/services.php
+++ config/services.php
@@ -6,8 +6,9 @@ return [
         'master' => env('KEEN_MASTER_KEY'),
         'write'  => env('KEEN_WRITE_KEY'),
         'addons' => [
-            'ip_to_geo' => true,
-            'ua_parser' => true,
+            'ip_to_geo'       => true,
+            'ua_parser'       => true,
+            'referrer_parser' => true,
         ],
     ],
 ];
