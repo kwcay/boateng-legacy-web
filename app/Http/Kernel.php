68efd2403817d5e3fa5650a15876d89fdd78dd9e
Frank <frank@frnk.ca>
2017-11-09 18:54:10 -0500

    Extending LaravelKeen class

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Frnkly\LaravelKeen\Middleware::class,
+        \App\Http\Middleware\TracksRequests::class,
     ];
 
     /**
---
+++ app/Http/Middleware/TracksRequests.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TracksRequests extends \Frnkly\LaravelKeen\Middleware
+{
+    /**
+     * Determines if the middleware should run or not.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Response $response
+     * @return bool
+     */
+    protected function shouldRun($request, $response) : bool
+    {
+        if (! parent::shouldRun($request, $response)) {
+            return false;
+        }
+
+        if (! $user = $request->user()) {
+            return true;
+        }
+
+        // Skip tracking for some users
+        if (strrpos($user->email, '@doraboateng.com') === 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
