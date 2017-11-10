4378a1799899004c15c0e3246d8526a52657b1ba
Frank <frank@frnk.ca>
2017-11-09 23:30:38 -0500

    cleaning up javascripts

--- resources/assets/js/app/api.js
+++ resources/assets/js/app/api.js
@@ -1,25 +1,26 @@
 
 window.ApiHelper = (function (csrf, logger) {
   // Set CSRF token
-  var token = csrf && csrf.content && csrf.content.length ? csrf.content : null;
+  const token = csrf && csrf.content && csrf.content.length ? csrf.content : null;
 
   /**
    * @param  {string} endpoint
    * @param  {string} method
    */
-  var makeRequest = function (endpoint, method) {
-    if (! token)
-      return logger.error("Invalid token.");
+  function makeRequest(endpoint, method = 'GET') {
+    if (!token) {
+      return logger.error('Invalid token.');
+    }
 
-    method = (method || "GET").toUpperCase();
+    logger.log(`TODO: ${method} request to ${endpoint}`);
 
-    logger.log("TODO: " + method + " request to " + endpoint);
-  };
+    return null;
+  }
 
   return {
     req: makeRequest,
     log: logger.log,
     warn: logger.warn,
-    error: logger.error
+    error: logger.error,
   };
-})(document.getElementsByName("csrf-token")[0], window.console);
+})(document.getElementsByName('csrf-token')[0], window.console);
