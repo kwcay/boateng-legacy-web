cbb33ff198125819d1101271c66dac96e54f3783
Frank <frank@frnk.ca>
2017-11-17 08:20:51 -0500

    Updating css

--- resources/views/partials/css.blade.php
+++ resources/views/partials/css.blade.php
@@ -1,6 +1,4 @@
 
-<!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Crimson+Text:100,400,700">
 <link rel="stylesheet" type="text/css" href="{{ App\Utilities::rev('app.css') }}">
 @if (Auth::check())
 <link rel="stylesheet" type="text/css" href="{{ App\Utilities::rev('user.css') }}">
--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -1,4 +1,5 @@
 
+<!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
 <script src="{{ App\Utilities::rev('app.js') }}" type="text/javascript"></script>
 @if (Auth::check())
 <script src="{{ App\Utilities::rev('user.js') }}" type="text/javascript"></script>
